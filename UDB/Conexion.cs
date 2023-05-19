50c581a2adcb5e2a2a12f9d2ebe354381dc4397a
Carlos_Alberto_Sanchez <95440504+CallmeCarlos503@users.noreply.github.com>
2023-05-19 15:02:26 -0600

    Initial commit

---
+++ UDB/Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDB
+{
+   public class Conexion
+    {
+        public SqlConnection connect= new SqlConnection(@"DESKTOP-70BPDVD\SQLEXPRESS;Database=UDB;Trusted_Connection=True");
+        public DataTable listaCarreras()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType= CommandType.TableDirect;
+            cmd.CommandText = "SELECT * FROM CARRERAS";
+            cmd.Connection = connect;
+            SqlDataAdapter da= new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+    }
+}
---
+++ UDB/formulario.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UDB
+{
+    /// <summary>
+    /// Lógica de interacción para formulario.xaml
+    /// </summary>
+    public partial class formulario : Window
+    {
+        public formulario()
+        {
+            InitializeComponent();
+            cbo_tipo.Items.Add("Queja");
+            cbo_tipo.Items.Add("Opinion");
+            cbo_tipo.Items.Add("Felicitaciones");
+            cbo_tipo.Items.Add("inscripcion");
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
